dc71acffa42e69c4756f8189646fdd8b62ac78bc
Nataliia-Bilets <natalia_bilets@ukr.net>
2023-01-03 00:23:51 +0200

    hw-09

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,48 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
-}
+  import Notiflix from 'notiflix';
+	import 'notiflix/dist/notiflix-3.2.5.min.css';
+	
+	function createPromise(position, delay) {
+	return new Promise((resolve, reject) => {
+	setTimeout(() => {
+	const shouldResolve = Math.random() > 0.3;
+	if (shouldResolve) {
+	resolve({ position, delay });
+	} else {
+	reject({ position, delay });
+	}
+	}, delay);
+	});
+	};
+	
+	const form = document.querySelector('.form');
+	const delay = document.querySelector('[name="delay"]');
+	const step = document.querySelector('[name="step"]');
+	const amount = document.querySelector('[name="amount"]');
+		
+	form.addEventListener('submit', submitCreatePromises);
+		
+	function submitCreatePromises(e) {
+	
+	e.preventDefault();
+	
+	const delayVal =  Number(delay.value);
+	const stepVal = Number(step.value);
+	const amountVal = Number(amount.value);
+	
+	for (let i = 1; i <= amountVal; i++) {
+	createPromise(i, delay)
+	.then(({ position, delay }) => {
+	Notiflix.Notify.success(
+	`✅ Fulfilled promise ${i} in ${delay}ms`
+	);
+	})
+	.catch(({ position, delay }) => {
+	Notiflix.Notify.failure(
+	`❌ Rejected promise ${i} in ${delay}ms`
+	);
+	});
+	delay += stepVal;
+    };
+};
+	
+	
